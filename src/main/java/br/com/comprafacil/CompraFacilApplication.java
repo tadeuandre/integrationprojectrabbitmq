743913a75c6f32f2797c3963850926f9d0067919
Tadeu Oliveira <tadeuandre@gmail.com>
2018-12-05 02:58:49 -0200

    Project Created

---
+++ src/main/java/br/com/comprafacil/CompraFacilApplication.java
@@ -0,0 +1,12 @@
+package br.com.comprafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompraFacilApplication.class, args);
+	}
+}
