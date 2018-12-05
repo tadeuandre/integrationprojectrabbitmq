e00a36cb2729d519208c7aa88d06b3597821f3d7
Tadeu Oliveira <tadeuandre@gmail.com>
2018-12-05 03:49:13 -0200

    Classes related to Product Domain

---
+++ src/main/java/br/com/comprafacil/domain/model/Produto.java
@@ -0,0 +1,42 @@
+package br.com.comprafacil.domain.model;
+
+public class Produto {
+
+	private String id;
+	private String descricao;
+	private String valorAtual;
+	private Long quantidadeEstoque;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getValorAtual() {
+		return valorAtual;
+	}
+
+	public void setValorAtual(String valorAtual) {
+		this.valorAtual = valorAtual;
+	}
+
+	public Long getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Long quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+}
---
+++ src/main/java/br/com/comprafacil/domain/services/ProdutoService.java
@@ -0,0 +1,5 @@
+package br.com.comprafacil.domain.services;
+
+public class ProdutoService {
+
+}
